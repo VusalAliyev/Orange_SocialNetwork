c9688b386b341c35484daa9ea92996a6bb612933
Vusal Aliyev <vusalaliev02@gmail.com>
2023-04-14 01:29:45 +0400

    Register operations done

--- src/SocialNetwork.API/Controllers/AccountController.cs
+++ src/SocialNetwork.API/Controllers/AccountController.cs
@@ -1,13 +1,56 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Azure.Core.Pipeline;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Domain.DTOs;
+using SocialNetwork.Domain.Entites;
 
 namespace SocialNetwork.API.Controllers
 {
-    public class AccountController
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
     {
-        //public IActionResult Login()
-        //{
-        //    return NoContent();
-        //}
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            AppUser user = new AppUser
+            {
+                FullName=registerDto.FullName,
+                Email=registerDto.Email,
+                UserName=registerDto.UserName
+            };
+
+            await _userManager.CreateAsync(user,registerDto.Password);
+
+            await _signInManager.SignInAsync(user, true);
+
+            return Ok(user);
+        }
+    
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return NoContent();
+        }
     }
 }
--- src/SocialNetwork.Domain/DTOs/RegisterDto.cs
+++ src/SocialNetwork.Domain/DTOs/RegisterDto.cs
@@ -18,7 +18,7 @@ namespace SocialNetwork.Domain.DTOs
         public string Email { get; set; }
         [Required, DataType(DataType.Password)]
         public string Password { get; set; }
-        [Required, DataType(DataType.Password),Compare(nameof(Password))]
+        [Required, DataType(DataType.Password), Compare(nameof(Password))]
         public string ConfirmPassword { get; set; }
     }
 }
